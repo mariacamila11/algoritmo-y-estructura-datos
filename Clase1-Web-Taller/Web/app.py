00295462286b654a9a6641a8b35a5c19b6cb0230
mariacamila11 <herreram.maria@uces.edu.co>
2020-11-21 00:31:00 -0500

    taller-reto

---
+++ Clase1-Web-Taller/Web/app.py
@@ -0,0 +1,54 @@
+
+from flask import Flask, request, make_response, redirect, render_template, url_for
+import utilidades as helper
+# se crea un objeto del tipo app
+app = Flask(__name__)
+fileNameCredential = 'usuarios.csv'
+data = helper.leerArchivo(fileNameCredential)
+
+@app.route('/')
+def baseRoute():
+    return redirect(url_for('login'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/lugares')
+def lugares():
+    return render_template('lugares.html')
+
+@app.route('/personajes')
+def personajes():
+    return render_template('personajes.html')
+
+@app.route('/success')
+def success():
+   return render_template('success.html')
+
+@app.route('/signin', methods = ['POST','GET'])
+def singin():
+
+    if request.method == 'POST':
+        helper.saveUser (data,fileNameCredential,request.form['name'],request.form ['pass'] )
+        return redirect(url_for('success'))
+    else: 
+        return render_template('signIn.html')
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    data = helper.leerArchivo(fileNameCredential)
+    if request.method == 'POST':
+        nameUser = request.form['name']
+        passUser = request.form ['pass']
+        output = helper.validatePassword (data, nameUser, passUser)
+        if (output == True):
+            return  redirect(url_for('home'))
+        elif (output == 'Usuario no registrado'):
+            return  redirect(url_for('singin'))
+        else:
+            return 'Falló proceso de autenticación'
+    else: 
+        return render_template('login.html')
+if __name__ == '__main__':
+    app.run()
